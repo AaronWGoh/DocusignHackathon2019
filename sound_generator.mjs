ff12d99e9a09b98e2d52e5df0c63914eea7529bb
AaronGoh <aaronwgoh5@gmail.com>
2019-06-08 21:32:40 -0700

    Initial Repository with node modules

---
+++ sound_generator.mjs
@@ -0,0 +1,15 @@
+import * as mm from '@magenta/music';
+
+//const model = new mm.MusicVAE('/path/to/checkpoint');
+const player = new mm.Player();
+
+model
+  .initialize()
+  .then(() => model.sample(1))
+  .then(samples => {
+    player.resumeContext();
+    player.start(samples[0])
+  });
+
+
+export function
